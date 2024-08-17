b62eaf7191a53a726551b7040cbb182316df75ef
conglytran0301 <congly.work@gmail.com>
2024-08-17 21:49:49 +0700

    V3/1

--- api.js
+++ api.js
@@ -38,7 +38,7 @@ const apiUrl = window.location.href;
 const apiReplacedURL = apiUrl.replace('#', '&');
 const apiFinalURL = new URLSearchParams(apiReplacedURL);
 
-var api_access_token = apiFinalURL.get('eyJraWQiOiJLQXA1dEdPRUw1clltKzRmVnJrRzRCaUVyM0todFdwVnFTa1VZZjZKblV3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzOTVhNjUyYy04MGExLTcwZWMtZDI0NS0yOGNlMjc2NmQ0NDAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNzIzOTAzOTQxLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTEuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTFfc0RLMGUzRlEwIiwiZXhwIjoxNzIzOTA3NTQxLCJpYXQiOjE3MjM5MDM5NDEsInZlcnNpb24iOjIsImp0aSI6IjRjNGVlZmMxLTU0YWYtNDMxOC1hM2RiLTcyZjMyMDM3OThkZSIsImNsaWVudF9pZCI6ImE0dWdkdnRmbDFiMDQzMXAxb3B0a2s4aDEiLCJ1c2VybmFtZSI6ImNvbmdseXRyYW4wNjAzIn0.nGUnRiVQF5X2zlNVWZsjjkLLunCbC_wPiy7mH5AT3-qID_dWNoQThlCABwqJSVqgaMrSfYF2tQnRCvVpirQbRn4L3yhVctZbksCpvYtDMy53zeSCEJU9XRySpxC15r44tcQOkhr0g3GJtZOcgd1LlwhZ4Vs2IVM6X04u3tky5-ujCSqMpjZNYUxKjguB8YUFFAtVxXHcBmv5Kk_RJ-VHn6AGDnKcUplSiAypZh2Q01eVN8xjYHO5um2yrC5wkTumL2UOhrFhHSkavnZPDU0eSeQtMIZP7I_lxfrnqjLhG_6iYijdKwJNyDO56s-s4QpDWL5dA2r5_jtYDodKQcYQ0Q&expires_in=3600');
+var api_access_token = apiFinalURL.get('eyJraWQiOiJLQXA1dEdPRUw1clltKzRmVnJrRzRCaUVyM0todFdwVnFTa1VZZjZKblV3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzOTVhNjUyYy04MGExLTcwZWMtZDI0NS0yOGNlMjc2NmQ0NDAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNzIzOTA0NTI1LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTEuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTFfc0RLMGUzRlEwIiwiZXhwIjoxNzIzOTA4MTI1LCJpYXQiOjE3MjM5MDQ1MjUsInZlcnNpb24iOjIsImp0aSI6ImFjZTYzMzc1LTc4ZjgtNGZmYS05MThmLTAyNzM3MGYzNjJkZSIsImNsaWVudF9pZCI6ImE0dWdkdnRmbDFiMDQzMXAxb3B0a2s4aDEiLCJ1c2VybmFtZSI6ImNvbmdseXRyYW4wNjAzIn0.dGpjC-eZ3aY74UC_zV8fe93p9j19scZSyw4aFXZfB3yGRLYOVCJCCDska_UHSYg4htCFyDlb_FxXW5w0AH2y596ukD4yAeFQJp-CsDTrI2n8l5T-b2besA3k1MgCrL5WvrMx4PmcvI3CJnPhNIRV2QaBssluTc7ELO2NfHCk0J7UCwO9WyxbXTDRt3vjMRpVwaLwWyck5Iwsn-buFSSCO_msMM-MuU5oEjArJ3TQ_H25crnpWTD02NEtYEGD6nmmcZ_tB4GrwAwqnj37cdqUIHZoxldlWq_YV-HoMC1neSlcbYu8hls2rYcxsv2QM03sQblu8tmlZ44TB__HPCKk5A');
 
 var sendData = (userName, email, subject, description) => {
     fetch(
@@ -53,13 +53,7 @@ var sendData = (userName, email, subject, description) => {
             }),
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': eyJraWQiOiJLQXA1dEdPRUw1clltKzRmVnJrRzRCaUVyM0todFdwVnFTa1VZZjZKblV3PSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzOTVhNjUyYy04MGExLTcwZWMt
-                ZDI0NS0yOGNlMjc2NmQ0NDAiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6ImF3cy5jb2duaXRvLnNpZ25pbi51c2VyLmFkbWluIHBob25lIG9wZW5pZCBwcm9maWxlIGVtYWlsIi
-                wiYXV0aF90aW1lIjoxNzIzOTA0NTI1LCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTEuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTFfc0RLMGUzRlEwI
-                iwiZXhwIjoxNzIzOTA4MTI1LCJpYXQiOjE3MjM5MDQ1MjUsInZlcnNpb24iOjIsImp0aSI6ImFjZTYzMzc1LTc4ZjgtNGZmYS05MThmLTAyNzM3MGYzNjJkZSIsImNsaWVudF9pZCI6ImE0d
-                WdkdnRmbDFiMDQzMXAxb3B0a2s4aDEiLCJ1c2VybmFtZSI6ImNvbmdseXRyYW4wNjAzIn0.dGpjC-eZ3aY74UC_zV8fe93p9j19scZSyw4aFXZfB3yGRLYOVCJCCDska_UHSYg4htCFyDlb_Fx
-                XW5w0AH2y596ukD4yAeFQJp-CsDTrI2n8l5T-b2besA3k1MgCrL5WvrMx4PmcvI3CJnPhNIRV2QaBssluTc7ELO2NfHCk0J7UCwO9WyxbXTDRt3vjMRpVwaLwWyck5Iwsn-buFSSCO_msMM-MuU
-                5oEjArJ3TQ_H25crnpWTD02NEtYEGD6nmmcZ_tB4GrwAwqnj37cdqUIHZoxldlWq_YV-HoMC1neSlcbYu8hls2rYcxsv2QM03sQblu8tmlZ44TB__HPCKk5A  // Sử dụng đúng header Authorization cho access token
+                'Authorization': api_access_token  // Sử dụng đúng header Authorization cho access token
             },
         }
     )
