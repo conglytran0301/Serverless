a7645abefab649ce6a25b4e9deaf356cd0f42e5a
conglytran0301 <congly.work@gmail.com>
2024-08-17 22:22:52 +0700

    V3.2

--- main.js
+++ main.js
@@ -53,7 +53,7 @@ cognitoidentityserviceprovider.getUser(params, function(err, data) {
     }
 });*/
 
-const url = window.location.href;
+/*const url = window.location.href;
 const replacedURL = url.replace('#', '&');
 const finalURL = new URLSearchParams(replacedURL);
 var accessToken = finalURL.get('access_token');
@@ -98,4 +98,60 @@ cognitoidentityserviceprovider.getUser(params, function(err, data) {
         document.getElementById('userNameInput').value = UserName;
         document.getElementById('userEmailInput').value = UserEmail;
     }
+});*/
+
+const url = window.location.href;
+const replacedURL = url.replace('#', '&');
+const finalURL = new URLSearchParams(replacedURL);
+var accessToken = finalURL.get('access_token');
+var idToken = finalURL.get("id_token");
+var UserName, UserEmail;
+
+aws_region = 'ap-southeast-1';
+AWS.config.region = aws_region;
+
+AWS.config.apiVersions = {
+    cognitoidentityserviceprovider: '2016-04-18'
+};
+
+var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
+
+var params = {
+    AccessToken: accessToken
+};
+
+cognitoidentityserviceprovider.getUser(params, function(err, data) {
+    if (err) {
+        // Nếu có lỗi, điều hướng về trang đăng nhập hoặc trang chủ
+        window.location.href = 'https://conglytran0301.github.io/Serverless/';
+    } else {
+        console.log(data);
+
+        // Lấy thông tin người dùng từ phản hồi của Cognito
+        for (var i = 0; i < data.UserAttributes.length; i++) {
+            if (data.UserAttributes[i].Name === 'name') {
+                UserName = data.UserAttributes[i].Value;
+            }
+            if (data.UserAttributes[i].Name === 'email') {
+                UserEmail = data.UserAttributes[i].Value;
+            }
+        }
+
+        // Kiểm tra và gán giá trị cho các phần tử HTML
+        if (document.getElementById('userNameDisplay')) {
+            document.getElementById('userNameDisplay').innerHTML = UserName;
+        }
+        if (document.getElementById('userEmailDisplay')) {
+            document.getElementById('userEmailDisplay').innerHTML = UserEmail;
+        }
+
+        // Kiểm tra và gán giá trị cho các trường đầu vào của form
+        if (document.getElementById('userNameInput')) {
+            document.getElementById('userNameInput').value = UserName;
+        }
+        if (document.getElementById('userEmailInput')) {
+            document.getElementById('userEmailInput').value = UserEmail;
+        }
+    }
 });
+
