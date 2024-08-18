32f58a69d7c7bb2f51c55351178f18c09fc9430d
conglytran0301 <congly.work@gmail.com>
2024-08-18 08:22:48 +0700

    V3.4

--- api.js
+++ api.js
@@ -35,7 +35,7 @@ var sendData = (userName,email, subject, description)=>{
         .catch(error => console.log('error', error));
 }*/
 
-/*const apiUrl = window.location.href;
+const apiUrl = window.location.href;
 const apiReplacedURL = apiUrl.replace('#', '&');
 const apiFinalURL = new URLSearchParams(apiReplacedURL);
 
@@ -69,34 +69,3 @@ var sendData = (userName, email, subject, description) => {
     })
     .catch(error => console.error('Error:', error));
 }
-*/
-
-var sendData = (userName, email, subject, description) => {
-    fetch(
-        'https://7s5pb02ska.execute-api.ap-southeast-1.amazonaws.com/Dev/serverless',
-        {
-            method: "POST",
-            body: JSON.stringify({
-                "name": userName,
-                "email": email,
-                "subject": subject,
-                "description": description
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': api_access_token  // Sử dụng đúng header Authorization cho access token
-            },
-        }
-    )
-    .then(response => response.json())
-    .then(result => {
-        if (result['statusCode'] === 200) {
-            alert("Successfully Sent the Message");
-        } else {
-            alert("Error in sending message");
-            console.error(result);
-        }
-    })
-    .catch(error => console.error('Error:', error));
-}
-
