44faf949e1a16a48415972444f8a63eeced586e8
conglytran0301 <congly.work@gmail.com>
2024-08-18 15:25:44 +0700

    v5.1.2

--- Lambda Function Code/lambda_code.py
+++ Lambda Function Code/lambda_code.py
@@ -1,71 +1,93 @@
-import json
+import json  # Đảm bảo đã import json
 import boto3
 import uuid
 
-# Create DynamoDB resource
+# Tạo tài nguyên DynamoDB
 dynamodb = boto3.resource('dynamodb')
 
-# Change - the name of the DynamoDB table
+# Đặt tên bảng DynamoDB
 table = dynamodb.Table('FCJ-DynamoDB')
 
-# Create an SNS Client
+# Tạo một SNS Client
 client_sns = boto3.client('sns')
 
-# This function will be triggered by API Gateway
+# Hàm này sẽ được kích hoạt bởi API Gateway
 def lambda_handler(event, context):
+    try:
+        # Kiểm tra sự tồn tại của các key bắt buộc
+        required_keys = ['name', 'email', 'subject', 'description']
+        for key in required_keys:
+            if key not in event:
+                raise KeyError(f"Missing required key: {key}")
 
-    # Create a user ID
-    id = str(uuid.uuid4())
+        # Tạo một ID người dùng
+        id = str(uuid.uuid4())
 
-    # Send Message to SNS
-    handle_sns(id, event)
-    
-    # Store data to DynamoDB
-    statusCode = handle_dynamo_db(id, event)
-
-    return{
-        'statusCode': statusCode,
-    }
-
-# Publish a message to SNS
-def handle_sns(id, event):
-    sns_message = """
-        You got a new Message from https://conglytran0301.github.io/Serverless/
-        The message is as follows
+        # Gửi tin nhắn đến SNS
+        handle_sns(id, event)
         
-        id      : {id}
-        Name    : {userName}
-        email   : {email}
-        Message : {message}
-        Subject : {subject}
-        """.format(
-            id=id,
-            userName=event['name'], 
-            email=event['email'],
-            message=event['description'], 
-            subject=event['subject'],
-        )
-        
-    client_sns.publish(
+        # Lưu trữ dữ liệu vào DynamoDB
+        statusCode = handle_dynamo_db(id, event)
 
-        # Change - the ARN to the ARN of your SNS
-        TopicArn='arn:aws:sns:ap-southeast-1:336760284039:FCJ-SNSTopic:fdaebf41-4539-4878-bc77-b05fd2be490a',
-        Message= sns_message,
-        Subject= event['subject']
-    )
-
-
-# Add an Item to DynamoDB Table
-def handle_dynamo_db(id, event):
-    response = table.put_item(
-        Item={
-            'id': id,
-            'name': event['name'],
-            'email': event['email'],
-            'subject': event['subject'],
-            'description': event['description'],
+        return {
+            'statusCode': statusCode,
         }
-    )
-
-    return response['ResponseMetadata']['HTTPStatusCode']
     
+    except KeyError as ke:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f"Missing required key: {str(ke)}")
+        }
+
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error occurred: ' + str(e))
+        }
+
+# Gửi một tin nhắn đến SNS
+def handle_sns(id, event):
+    try:
+        sns_message = f"""
+            You got a new Message from https://conglytran0301.github.io/Serverless/
+            The message is as follows
+            
+            id      : {id}
+            Name    : {event['name']}
+            email   : {event['email']}
+            Message : {event['description']}
+            Subject : {event['subject']}
+        """
+        
+        client_sns.publish(
+            # Thay đổi ARN với ARN của SNS của bạn
+            TopicArn='arn:aws:sns:ap-southeast-1:336760284039:FCJ-SNSTopic',
+            Message=sns_message,
+            Subject=event['subject']
+        )
+    
+    except KeyError as ke:
+        print(f"Key error: {ke}")
+        raise
+    
+    except Exception as e:
+        print(f"An error occurred when sending SNS: {e}")
+        raise
+
+# Thêm một mục vào bảng DynamoDB
+def handle_dynamo_db(id, event):
+    try:
+        response = table.put_item(
+            Item={
+                'id': id,
+                'name': event['name'],
+                'email': event['email'],
+                'subject': event['subject'],
+                'description': event['description'],
+            }
+        )
+        return response['ResponseMetadata']['HTTPStatusCode']
+    
+    except Exception as e:
+        print(f"An error occurred when writing to DynamoDB: {e}")
+        raise
